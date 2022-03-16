4d8aba684bb52c50243b555e543e2de85908026a
unknown <sonarrohit7@gmail.com>
2022-03-16 15:16:10 +0530

    5 Commit

---
+++ src/com/Harman/Harman2/Check.java
@@ -0,0 +1,19 @@
+package com.Harman.Harman2;
+
+public class Check {
+
+    public static void main(String[] args) {
+
+        try {
+            int x=10 ,y=0,z;
+            z=x/y;
+            System.out.println(z);
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+
+    }
+
+}
